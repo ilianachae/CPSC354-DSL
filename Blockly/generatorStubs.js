4e422c56fe65e6047965b540fd8fd31d8f806870
Jaden Suh <92055248+JadenSuh@users.noreply.github.com>
2023-09-24 19:00:44 -0400

    Delete Blockly directory

--- Blockly/generatorStubs.js
+++
@@ -1,9 +0,0 @@
-Blockly.JavaScript['element'] = function(block) {
-    // Retrieve the chemical symbol entered by the user
-    const symbol = Blockly.JavaScript.valueToCode(block, 'Symbol', Blockly.JavaScript.ORDER_ATOMIC);
-  
-    // Generate code to represent the chemical symbol entered
-    const code = `${symbol}`;
-  
-    return [code, Blockly.JavaScript.ORDER_ATOMIC];
-  };
