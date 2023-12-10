cbc9d8af9fedc575c9d3925f0a9386a89f30b98c
ILIANA <124848987+ilianachae@users.noreply.github.com>
2023-12-09 21:27:25 -0800

    Update defaultBlocks.js

--- defaultBlocks.js
+++ defaultBlocks.js
@@ -1,7 +1,7 @@
 var xmlString = '<xml xmlns="https://developers.google.com/blockly/xml">' +
   '<block type="chemical_equation" id="|*nT426^Js?MG3aO+bXD" x="68" y="78">' +
   '<field name="EquationType">=></field>' +
-  '<value name="REACTANTS"><block type="reactants_or_products">
+  '<value name="REACTANTS"><block type="reactants_or_products"> +
   '<block type="ch4"></block>' +
   '</block></value>' +
   '<value name="PRODUCTS"><block type="reactants_or_products">' + 
