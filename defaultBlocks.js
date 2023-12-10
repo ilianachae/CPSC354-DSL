f7f605b2725f1c5d9535a446de7518773ed40e22
ILIANA <124848987+ilianachae@users.noreply.github.com>
2023-12-09 20:46:43 -0800

    Update defaultBlocks.js

--- defaultBlocks.js
+++ defaultBlocks.js
@@ -1,15 +1,6 @@
-var xmlString = '<xml xmlns="https://developers.google.com/blockly/xml">' +
-    '<block type="chemical_equation" id="|*nT426^Js?MG3aO+bXD" x="68" y="78">' +
-        '<field name="EquationType">=></field>' +
-        '<value name="REACTANTS">' +
-            '<block type="ch4"></block>' +
-        '</value>' +
-        '<value name="PRODUCTS">' +
-            '<block type="h2o"></block>' +
-            '<block type="co2"></block>' +
-        '</value>' +
-    '</block>' +
-'</xml>';
+var xmlString = '<xml xmlns="https://developers.google.com/blockly/xml"><block type="chemical_equation" id="|*nT426^Js?MG3aO+bXD" x="68" y="78"><field name="EquationType">=></field><value name="REACTANTS"><block type="reactants_or_products"></block></value><value name="PRODUCTS"><block type="reactants_or_products"></block></value></block></xml>';
+
+
 
 var parser = new DOMParser();
 var xmlDoc = parser.parseFromString(xmlString, "text/xml");
