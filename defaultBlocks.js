c453decf205ff1f48135fb618b5ee5fb23b95eca
ILIANA <124848987+ilianachae@users.noreply.github.com>
2023-12-09 21:40:06 -0800

    Update defaultBlocks.js

--- defaultBlocks.js
+++ defaultBlocks.js
@@ -2,14 +2,15 @@ var xmlString = '<xml xmlns="https://developers.google.com/blockly/xml">' +
   '<block type="chemical_equation" id="|*nT426^Js?MG3aO+bXD" x="68" y="78">' +
   '<field name="EquationType">=></field>' +
   '<value name="REACTANTS"><block type="reactants_or_products"> +
-   '<value name="Element1"><block type="element_symbol"><field name="NAME">C</field></block></value>' +
-   '<value name="Element2"><block type="element_symbol"><field name="NAME">H4</field></block></value>' +
+  //  '<value name="Element1"><block type="element_symbol"><field name="NAME">C</field></block></value>' +
+  // ''+
+  //  '<value name="Element2"><block type="element_symbol"><field name="NAME">H4</field></block></value>' +
   '</block></value>' +
   '<value name="PRODUCTS"><block type="reactants_or_products">' + 
-    '<block type="element_symbol"><field name="NAME">CH4</field></block></value>' +
-    '<value name="Element2"><block type="element_coefficient"><field name="NUMBER">2</field>' +
-    '<value name="COEFFICIENT"><block type="element_symbol"><field name="NAME">O2</field></block></value>' +
-    '</block></value></block></value>' +
+    // '<block type="element_symbol"><field name="NAME">CH4</field></block></value>' +
+    // '<value name="Element2"><block type="element_coefficient"><field name="NUMBER">2</field>' +
+    // '<value name="COEFFICIENT"><block type="element_symbol"><field name="NAME">O2</field></block></value>' +
+    // '</block></value></block></value>' +
   '</block></value>' +
   '</block>' +
   '</xml>';
